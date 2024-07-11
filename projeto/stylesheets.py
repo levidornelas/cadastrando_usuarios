39cd3afb7d7ca8ce829ebeee14ddc1ed983f3eb5
Levi <levidornelas2006@gmail.com>
2024-07-10 23:39:33 -0300

    ajustando CSS

--- projeto/stylesheets.py
+++ projeto/stylesheets.py
@@ -3,27 +3,27 @@ from PyQt6.QtWidgets import QMainWindow
 def apply_stylesheets(widget):
   StyleSheet = ("""
     QWidget {
-        background-color: #244855; /* cor principal de fundo */
+        background-color: #013F3E; /* cor principal de fundo */
         font-family: Arial, sans-serif;
         font-size: 14px;
-        color: #FBE900; /* cor do texto */
+        color: #E9E0CE; /* cor do texto */
     }
     QLabel {
-        color: #FBE900; /* cor do texto dos labels */
+        color: #E9E0CE; /* cor do texto dos labels */
         font-weight: bold;
     }
     QLineEdit {
         padding: 10px;
-        border: 1px solid #90AEAD; /* cor da borda */
+        border: 1px solid #2B3036; /* cor da borda */
         border-radius: 5px;
         font-size: 14px;
-        background-color: #874F41; /* cor de fundo dos inputs */
-        color: #FBE900; /* cor do texto dos inputs */
+        background-color: #2B3036; /* cor de fundo dos inputs */
+        color: #E9E0CE; /* cor do texto dos inputs */
     }
     
     QPushButton {
-        background-color: #E64833; /* cor de fundo dos botões */
-        color: #ffffff; /* cor do texto dos botões */
+        background-color: #3FA796; /* nova cor de fundo dos botões */
+        color: #E9E0CE; /* cor do texto dos botões */
         padding: 10px 20px; /* Aumenta o padding horizontal para centralizar o texto */
         font-size: 16px;
         border: none;
@@ -33,18 +33,18 @@ def apply_stylesheets(widget):
     }
     
     QPushButton:hover {
-        background-color: #874F41; /* cor de fundo dos botões ao passar o mouse */
+        background-color: #2B3036; /* cor de fundo dos botões ao passar o mouse */
     }
     QTableWidget {
-        background-color: #90AEAD; /* cor de fundo da tabela */
-        border: 1px solid #E64833; /* cor da borda da tabela */
+        background-color: #2B3036; /* cor de fundo da tabela */
+        border: 1px solid #3FA796; /* nova cor da borda da tabela */
         border-radius: 5px;
         font-size: 14px;
-        color: #244855; /* cor do texto da tabela */
+        color: #E9E0CE; /* cor do texto da tabela */
     }
     QHeaderView::section {
-        background-color: #E64833; /* cor de fundo do cabeçalho da tabela */
-        color: #FBE900; /* cor do texto do cabeçalho da tabela */
+        background-color: #3FA796; /* nova cor de fundo do cabeçalho da tabela */
+        color: #E9E0CE; /* cor do texto do cabeçalho da tabela */
         padding: 5px;
         border: none;
     }
